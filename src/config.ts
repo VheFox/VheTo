78e3390ad36ce26359f5dae9968307463fc6ed7e
VheFox <VheFox@outlook.com>
2025-11-26 12:17:36 +0800

    修改

--- src/config.ts
+++ src/config.ts
@@ -165,16 +165,16 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "Fengyasong", //"ZenMaruGothic-Medium",
+			fontFamily: "SourceHanSerifSC-Medium", //"Fengyasong", //"SourceHanSansCN-Medium.otf", //"ZenMaruGothic-Medium", //"WenKai-Bold",
 			fontWeight: "400",
-			localFonts: ["Fengyasong.ttf"], //["ZenMaruGothic-Medium.ttf"],
+			localFonts: ["SourceHanSerifSC-Medium.otf"], //["Fengyasong.ttf"],  //["SourceHanSansCN-Medium.otf"],  //["ZenMaruGothic-Medium.ttf"], //["WenKai-Bold.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
-			fontFamily: "Fengyasong", //"WenKai-Bold",  //"萝莉体 第二版",
+			fontFamily: "Fengyasong", //"WenKai-Bold",
 			fontWeight: "500",
-			localFonts: ["Fengyasong.ttf"], //["WenKai-Bold.ttf"],  //["萝莉体 第二版.ttf"],
+			localFonts: ["Fengyasong.ttf"], //["WenKai-Bold.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 	},
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,12 +1,12 @@
-# 你好呀ヾ(•ω•`)o，你可以叫我姀 (hé)
+# 你好呀ヾ(•ω•`)o  你可以叫我姀 (hé)
 
-### 我的兴趣：
+## 我的兴趣：
 - 追番，[听歌](https://c6.y.qq.com/base/fcgi-bin/u?__=h68dWBtlPXoU)，发呆，装机（一年一次就够了QAQ），睡觉(￣o￣) . z Z
-### 网站资料：
+## 网站资料：
 - **短链**：[Vhe.to](https://vhe.to)
 - **永久链接**：[VheFox.com](https://vhefox.com)
-### 狐言乱语：
+## 狐言乱语：
 - 什么时候才能成为大佬呀呜呜呜~~~~可怜我只是一个什么都不会的小菜鸡QAQ，发财请带上我！！！~~~~
-### 其他：
+## 其他：
 - **Typecho站**：[https://vfox.asia](https://vfox.asia/) 2026年9月到10月到期，带评论系统
 - **联系我**：[i@vfox.asia](mailto:i@vfox.asia)
